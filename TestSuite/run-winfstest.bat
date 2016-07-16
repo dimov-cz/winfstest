d405b9d76a5b55fee3be03948941d0bcfd1b8ad7
Liryna <soultaikerdidu@gmail.com>
2016-07-16 08:22:19 +0200

    Look for python in HKLM

--- TestSuite/run-winfstest.bat
+++ TestSuite/run-winfstest.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set PythonRegKey=HKCU\SOFTWARE\Python\PythonCore\2.7
+set PythonRegKey=HKLM\SOFTWARE\Python\PythonCore\2.7
 reg query %PythonRegKey%\InstallPath /ve >nul 2>&1 || (echo Cannot find Windows Python >&2 & exit /b 1)
 for /f "tokens=2,*" %%i in ('reg query %PythonRegKey%\InstallPath /ve ^| findstr Default') do (
     set PythonInstallPath=%%j
